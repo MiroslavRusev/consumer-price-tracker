e5cd438d97dacd9b2a7532558e28e5fa42ea493d
miroslav.rusev <miroslav.rusev@living.homes>
2025-07-30 16:14:49 +0300

    fix: Fixing period label.

--- src/components/structural/controlSection.svelte
+++ src/components/structural/controlSection.svelte
@@ -15,7 +15,7 @@
 
 	// Import stores and constants for reset functionality
 	import { selectedFoods, selectedFuel, selectedUtilityItems, foodStore, fuelStore, utilityStore } from '$lib/stores';
-	import { appDefaults, fuelItems as fuelItemsConstants } from '$lib/constants';
+	import { timeRanges, appDefaults, fuelItems as fuelItemsConstants } from '$lib/constants';
 
 	function resetToDefaults() {
 		selectedRange = appDefaults.range;
@@ -61,7 +61,7 @@
 										d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
 									></path>
 								</svg>
-								{selectedRange}
+								{ timeRanges.find((range) => range.id === selectedRange)?.label }
 							</span>
 							{#if selectedCount.foods > 0}
 								<span
