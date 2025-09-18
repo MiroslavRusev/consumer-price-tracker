9cbfe2c958f9941f5332d44f0643db34b2756c57
miroslav.rusev <miroslav.rusev@living.homes>
2025-09-18 14:35:26 +0300

    feat: adding vercel analytics.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script>
 	let { children } = $props();
 	import '../styles/input.css';
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import Footer from '../components/structural/Footer.svelte';
 	import Header from '../components/structural/Header.svelte';
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Header />
